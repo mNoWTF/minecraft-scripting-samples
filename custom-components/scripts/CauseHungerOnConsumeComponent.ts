4235a33b26534cee9917db4c89a0e8f8a08b0d7e
mNo <79966524+mNoWTF@users.noreply.github.com>
2026-06-16 15:19:07 +0200

    Delete custom-components directory

--- custom-components/scripts/CauseHungerOnConsumeComponent.ts
+++
@@ -1,8 +0,0 @@
-import { ItemComponentConsumeEvent, ItemCustomComponent } from "@minecraft/server";
-
-export class CauseHungerOnConsumeComponent implements ItemCustomComponent {
-  onConsume(arg: ItemComponentConsumeEvent) {
-    arg.source.addEffect("minecraft:hunger", 600);
-    arg.source.applyDamage(0.5);
-  }
-}
--- custom-components/scripts/NightVisionConsumeComponent.ts
+++
@@ -1,7 +0,0 @@
-import { ItemComponentConsumeEvent, ItemCustomComponent } from "@minecraft/server";
-
-export class NightVisionConsumeComponent implements ItemCustomComponent {
-  onConsume(arg: ItemComponentConsumeEvent) {
-    arg.source.addEffect("minecraft:night_vision", 600);
-  }
-}
