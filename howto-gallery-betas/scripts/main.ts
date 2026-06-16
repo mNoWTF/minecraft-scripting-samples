1e864dac579b3bb50c979ec373cda289f09424ea
mNo <79966524+mNoWTF@users.noreply.github.com>
2026-06-16 15:20:13 +0200

    Delete howto-gallery-betas directory

--- howto-gallery-betas/scripts/main.ts
+++
@@ -1,8 +0,0 @@
-import SampleManager from "./SampleManager";
-import * as serverSampleLibrary from "./ServerSampleLibrary";
-import * as mcgtsl from "./Server-GameTestSampleLibrary";
-
-const sm = new SampleManager();
-
-serverSampleLibrary.register(sm);
-mcgtsl.register(sm);
